f5a9b4bf98be7f2205db8b5055cf33ae707a7267
andyholmes1210 <andy.holmes7662@gmail.com>
2023-01-21 22:55:31 +1300

    Change file from JS to Typescript

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navbar from "../src/components/Navbar";
+
+function App(){
+    return (
+        <>
+          <Navbar />
+        </>
+      )
+}
+
+
+export default App;
