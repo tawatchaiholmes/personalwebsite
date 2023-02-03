dc6999386891f0cb2c22a084f05183a05239614a
andyholmes1210 <andy.holmes7662@gmail.com>
2023-02-03 20:48:26 +1300

    WIP Css for Homepage

---
+++ my-app/src/components/home/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Home.css";
+import Data from "./Data";
+import Social from './Social';
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+
+          <div className="home__img"></div>
+        
+          <Data />
+    
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
