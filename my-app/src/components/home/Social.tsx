3829fba1b273ab5cb9e5e7af619b0c1955416a93
andyholmes1210 <andy.holmes7662@gmail.com>
2023-03-01 04:27:16 +1300

    Added Social into Contact section and added :focus on contact input form/Add WhatsApp number

--- my-app/src/components/home/Social.tsx
+++ my-app/src/components/home/Social.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react"
 
 const Social = () => {
   return (
     <div className="home__social">
-    <a href="https://github.com/andyholmes1210" className="home__social-icon" target="_blank">
+      <a
+        href="https://github.com/andyholmes1210"
+        className="home__social-icon"
+        target="_blank"
+      >
         <i className="uil uil-github"></i>
-    </a>
+      </a>
 
-    <a href="https://www.linkedin.com/in/tawatchai-holmes-56395a250/" className="home__social-icon" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/tawatchai-holmes-56395a250/"
+        className="home__social-icon"
+        target="_blank"
+      >
         <i className="uil uil-linkedin"></i>
-    </a>
-</div>
+      </a>
+    </div>
   )
 }
 
